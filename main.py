98b247ee9be0433112b169b607acac14a1bf453a
Ichigoz <mos-moji350@hotmail.com>
2023-02-21 13:50:56 +0700

    add binance-futures-connector

--- main.py
+++ main.py
@@ -4,7 +4,28 @@ import config
 # from connectors.binance_futures import BinanceFuturesClient
 from connectors.binance import BinanceClient
 from interface.root_component import Root
+from binance.cm_futures import CMFutures
 
+cm_futures_client = CMFutures()
+
+print(cm_futures_client.time())
+
+cm_futures_client = CMFutures(key=config.FUTURES_API_KEY, secret=config.FUTURES_API_SECRET,base_url='https://testnet.binancefuture.com')
+# Get account information
+print(cm_futures_client.account())
+
+# Post a new order
+# params = {
+#     'symbol': 'BTCUSDT',
+#     'side': 'SELL',
+#     'type': 'LIMIT',
+#     'timeInForce': 'GTC',
+#     'quantity': 0.002,
+#     'price': 59808
+# }
+
+response = cm_futures_client.new_order(**params)
+# print(response)
 
 # Create and configure the logger object
 
@@ -29,6 +50,7 @@ if __name__ == '__main__':  # Execute the following code only when executing mai
     # binance = BinanceFuturesClient(config.FUTURES_API_KEY, config.FUTURES_API_SECRET, testnet=True)
     binance = BinanceClient(config.FUTURES_API_KEY, config.FUTURES_API_SECRET, testnet=True, futures=True)
     # binance = BinanceClient(config.SPOT_API_KEY, config.SPOT_API_SECRET, testnet=True, futures=False)
-    
+    # client = Client(config.FUTURES_API_KEY, config.FUTURES_API_SECRET, testnet=True, futures=True)
+    # 
     root = Root(binance)
     root.mainloop()
